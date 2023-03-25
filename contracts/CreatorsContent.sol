58064b197258658fa67a3679507284802d15c60c
vmlopez <vmlopez@slashweb.com.mx>
2023-03-25 12:01:32 -0600

    Adding Payable functions to the contract

--- contracts/CreatorsContent.sol
+++ contracts/CreatorsContent.sol
@@ -25,13 +25,23 @@ contract CreatorsContent {
         address addr;
         string profilePic;
     }
+
+    struct ContentBuy {
+        address from;
+        address to;
+        uint256 idContent;
+        uint256 ammount;
+        bool isPayed;
+    }
+
     
     Content[] public Contents;
     Profile[] public Profiles;
+    ContentBuy[] public ContentBuys;
 
     Counters.Counter idCourses;
 
-    constructor() {
+    constructor() payable {
         idCourses.reset();
     }
 
@@ -161,6 +171,69 @@ contract CreatorsContent {
 
         return localProfile;
     }
+    
+    function getContractBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
 
+    function getAvailableBalance() public view returns (uint256) {
+        uint256 ammount;
+
+        for (uint256 i = 0; i < ContentBuys.length; i++) {
+            if (ContentBuys[i].to == msg.sender && ContentBuys[i].isPayed == false) {
+                ammount += ContentBuys[i].ammount;
+            }
+        }
+
+        return ammount;
+    }  
+
+    function getBuysFromAuthor() public view returns (ContentBuy[] memory) {
+        ContentBuy[] memory list;
+
+        uint256 index = 0;
+
+        for(uint256 i = 0; i < ContentBuys.length; i++) {
+            if (ContentBuys[i].to == msg.sender) {
+                list[i] = ContentBuys[i];
+                index++;
+            }
+        }
+
+        return list;
+    }
+
+    function getBalanceForSingleBuy(address payable addr, uint256 indexBuy) public payable {
+        
+        require(ContentBuys[indexBuy].to == addr && ContentBuys[indexBuy].isPayed == false);
+
+        addr.transfer(ContentBuys[indexBuy].ammount);
+        ContentBuys[indexBuy].isPayed = true;
+    } 
+
+    function getAllBalanceForCreator(address payable addr) public payable {
+        uint256 ammount = 0;
+
+        ammount = getAvailableBalance();
+
+        addr.transfer(ammount);
+
+        for (uint256 i = 0; i < ContentBuys.length; i++) {
+            ContentBuys[i].isPayed = true;
+        }
+    }
+
+    function buyContent(uint256 idCourse) payable public {
+
+        // Get the content entity that will be buyed
+        Content memory content = getCourseDetail(idCourse);
+
+        // Validate the price with the ammount to recieve
+        require(msg.value == content.price);
+
+        // Save transaction for that the users can retrieve their money
+        ContentBuys.push( ContentBuy(msg.sender, content.author, idCourse, msg.value, false));
+
+    }
 
 } 
